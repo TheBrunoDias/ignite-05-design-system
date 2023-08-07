c987bb2e1f1a0e57189d41ba03a44a18cd312a52
TheBrunoDias <brunosousadias@hotmail.com>
2023-08-07 15:57:21 -0300

    fix: config base path

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -30,7 +30,7 @@ const config = {
   },
   viteFinal: (config, {configType}) => {
     if(configType === 'PRODUCTION'){
-      config.base = '/ignite-05-design-system'
+      config.base = '/ignite-05-design-system/'
     }
 
     return config;
